c5758c37d109ed1eb80bb8c6dbad996818ee4b74
jcastaneda <jcastaneda@SIS-LAP102>
2024-03-25 21:19:43 -0600

    feat + refactor: New to-do input text state added and style of input was updated

--- src/components/TodoSearch/TodoSearch.js
+++ src/components/TodoSearch/TodoSearch.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoSearch.css';
 import TodoCreateButton from '../CreateTodoButton/CreateTodoButton';
 
 function TodoSearch() {
+  const [todoInput, setTodoInput] = useState('');
   return (
     <div className='todo-search-form'>
-      <input className='todo-input' placeholder='Add a new task...' autoFocus={true}/>
+      <input
+        className='todo-input'
+        placeholder='Add a new task...'
+        autoFocus={true}
+        onChange={(event) => setTodoInput(event.target.value)} />
       <TodoCreateButton />
     </div>
   )
