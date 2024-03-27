f10c6cd204beb23af7e6832061a8a33f7a31ba38
jcastaneda <jcastaneda@SIS-LAP102>
2024-03-26 20:09:36 -0600

    ?: is this the correct way?

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList/TodoList.js';
 import TodoHeader from './components/TodoHeader/TodoHeader.js';
 
+export const UserContext = React.createContext();
+
 function App() {
+  const [todoInput, setTodoInput] = useState('');
+
+
   return (
     <React.Fragment>
       <div className='todo-elements'>
-        <TodoHeader />
-        <TodoList />
+        <UserContext.Provider value={{todoInput, setTodoInput}}>
+          <TodoHeader />
+          <TodoList />
+        </UserContext.Provider>
       </div>
     </React.Fragment>
   );
--- src/components/TodoSearch/TodoSearch.js
+++ src/components/TodoSearch/TodoSearch.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
 import './TodoSearch.css';
 import TodoCreateButton from '../CreateTodoButton/CreateTodoButton';
+import { UserContext } from '../../App';
 
 function TodoSearch() {
-  const [todoInput, setTodoInput] = useState('');
+  const { todoInput, setTodoInput } = React.useContext(UserContext);
+
+  const handleInputChange = (event) => {
+    setTodoInput(event.target.value);
+  };
+
+  const handleEnterKeyDown = (event) => {
+    if (event.key === 'Enter' && todoInput.trim() !== '')
+    {
+      setTodoInput('');
+    }
+  }
+
   return (
     <div className='todo-search-form'>
       <input
         className='todo-input'
         placeholder='Add a new task...'
         autoFocus={true}
-        onChange={(event) => setTodoInput(event.target.value)} />
+        onChange={handleInputChange}
+        onKeyDown={handleEnterKeyDown} />
       <TodoCreateButton />
     </div>
   )
