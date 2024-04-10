c2961c20dd2e8ad0dff9068a61bec73f6b25d7b0
Juan David <jd.cas.dev@gmail.com>
2024-04-10 17:57:31 -0600

    feat: When adding a new todo, it depends on which filter is active, whether it is displayed or not

--- src/components/CreateTodoButton/CreateTodoButton.js
+++ src/components/CreateTodoButton/CreateTodoButton.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import './CreateTodoButton.css'
+import './CreateTodoButton.css';
+import { todoContext } from '../../App';
 
-function CreateTodoButton(props) {
+function CreateTodoButton() {
+  const { todoArray, setTodoArray, 
+          todoCount, setTodoCount,
+          todoFilterArray, setTodoFilterArray,
+          todoFilteredArray, setTodoFilteredArray,
+          todoInput } = React.useContext(todoContext);
+
+  const handleAddTodoButton = () => {
+    if (todoInput.trim() !== '')
+    {
+      const lastTodo = todoArray[todoArray.length - 1];
+      const formatedTodo = { id: lastTodo == undefined ? 1 : lastTodo.id+1, text: todoInput, completed: false }
+      const newArray = [...todoArray, formatedTodo];
+      setTodoArray(newArray);
+      
+      const selectedFilterElement = [...todoFilterArray].find(a => a.active == true);
+      switch(selectedFilterElement.text){
+        case 'All':
+          setTodoFilteredArray(newArray);
+          break;
+        case 'Active':
+          const newActiveArray = [...todoFilteredArray, formatedTodo]
+          setTodoFilteredArray(newActiveArray);
+          break;
+      }
+
+      setTodoCount(todoCount+1);
+      setTodoFilterArray(todoFilterArray);
+    }
+  }
+
   return (
     <button
       className='add-todo-btn'
-      type='submit'>Add</button>
+      type='submit'
+      onClick={handleAddTodoButton}>Add</button>
   )
 }
 
